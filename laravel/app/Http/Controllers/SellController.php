f212f4da31fdbfc9140ce06699b0334bd4bad100
hamaa-affix <hamaagive1108@gmail.com>
2021-02-27 20:39:17 +0900

    セレクトボックスの実装

--- laravel/app/Http/Controllers/SellController.php
+++ laravel/app/Http/Controllers/SellController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ItemCondition;
 
 class SellController extends Controller
 {
     public function showSellForm()
     {
-        return view('sell');
+        // orderBy()はasc(昇順) か desc(降順)を指定するが、指定しない場合はascとなる
+        $conditions = ItemCondition::orderBy('sort_no')->get();
+        return view('sell')->with('conditions', $conditions);
     }
 }
