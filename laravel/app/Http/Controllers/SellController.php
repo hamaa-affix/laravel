9ca9023b8f2ed250e1a58b26efda739da418c6f7
hamaa-affix <hamaagive1108@gmail.com>
2021-03-07 10:21:12 +0900

    商品出品モデル、コントローラー、ホームリクエスト作成

--- laravel/app/Http/Controllers/SellController.php
+++ laravel/app/Http/Controllers/SellController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Http\Requests\SellRequest;
 use App\Models\ItemCondition;
 use App\Models\PrimaryCategory;
+use App\Models\Item;
 
 class SellController extends Controller
 {
@@ -15,10 +18,28 @@ class SellController extends Controller
         $cotegories = PrimaryCategory::with('secondaryCategories')
                     ->orderBy('sort_no')
                     ->get();
-                    
+
         return view('sell')
                 ->with('conditions', $conditions)
                 ->with('categories', $cotegories);
+    }
+
+    public function sellItem(SellRequest $request)
+    {
+
+        $user = Auth::user();
+
+        $item                        = new Item();
+        $item->seller_id             = $user->id;
+        $item->name                  = $request->input('name');
+        $item->description           = $request->input('description');
+        $item->secondary_category_id = $request->input('category');
+        $item->item_condition_id     = $request->input('condition');
+        $item->price                 = $request->input('price');
+        //$item->state                 = Item::STATE_SELLING;
+        $item->save();
+
+        return redirect()->back()->with('status', '商品を出品しました。');
     }
 
 
