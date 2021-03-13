6ad9652eff98d5136cad0b4c91a34368b72c61d1
hamaa-affix <hamaagive1108@gmail.com>
2021-03-13 22:07:26 +0900

    検索機能実装

---
+++ laravel/app/Http/Controllers/ItemsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemsController extends Controller
+{
+    public function showItems(Request $request)
+    {
+        $items = Item::orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )
+            ->orderBy('id', 'DESC')
+            ->paginate(52);
+
+        return view('items.items')
+            ->with('items', $items);
+    }
+
+    public function showItemDetail (Item $item)
+    {
+        return view('items.item_detail', compact('item'));
+    }
+}
